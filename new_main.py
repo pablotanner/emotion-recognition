3f69ba4fa9ff3ce40c267923d1359ab9a4f471c8
Pablo Tanner <pablo.tanner@uzh.ch>
2024-05-07 14:46:19 +0200

    Optimized data loading,  added more models for score fusion
    Currently get 58.57% using prob stacking w lin regression

--- new_main.py
+++ new_main.py
@@ -1,4 +1,7 @@
 import numpy as np
+from keras import Sequential
+from keras.src.layers import Dense, Dropout
+from keras.src.utils import to_categorical
 from scipy.stats import mode
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
@@ -14,20 +17,38 @@ from sklearn.pipeline import Pipeline
 
 from src import DataLoader, evaluate_results
 
-data_loader = DataLoader("./data", "./data", exclude=['deepface', 'facenet', 'landmarks'])
+data_loader = DataLoader("./data", "./data", exclude=[])
 
 y = data_loader.emotions
 
 X_dict = data_loader.features
-spatial_features = np.concatenate([X_dict['landmarks_3d'], X_dict['vggface']], axis=1)
+
+def process_embeddings(embeddings):
+    # Apply PCA then StandardSCaler
+    pca = PCA(n_components=0.95)
+    pca.fit(embeddings)
+    embeddings = pca.transform(embeddings)
+
+    scaler = StandardScaler()
+    embeddings = scaler.fit_transform(embeddings)
+
+    return embeddings
+
+X_dict['sface'] = process_embeddings(X_dict['sface'])
+X_dict['facenet'] = process_embeddings(X_dict['facenet'])
+X_dict['arcface'] = process_embeddings(X_dict['arcface'])
+
+spatial_features = np.concatenate([X_dict['landmarks_3d'], X_dict['landmarks']], axis=1)
+embedded_features = np.concatenate([X_dict['sface'], X_dict['facenet'], X_dict['arcface']], axis=1)
 facs_features = np.concatenate([X_dict['facs_intensity'], X_dict['facs_presence']], axis=1)
 pdm_features = np.array(X_dict['nonrigid_face_shape'])
 hog_features = np.array(X_dict['hog'])
 
 
-X = np.concatenate([spatial_features, facs_features, pdm_features, hog_features], axis=1)
+X = np.concatenate([spatial_features, embedded_features, facs_features, pdm_features, hog_features], axis=1)
 
 spatial_index = spatial_features.shape[1]
+embedded_index = embedded_features.shape[1]
 facs_index = facs_features.shape[1]
 pdm_index = pdm_features.shape[1]
 hog_index = hog_features.shape[1]
@@ -37,35 +58,75 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 # Splitting data first
 
 X_spatial_train, X_spatial_test = X_train[:, :spatial_index], X_test[:, :spatial_index]
-X_facs_train, X_facs_test = X_train[:, spatial_index:spatial_index + facs_index], X_test[:, spatial_index:spatial_index + facs_index]
-X_pdm_train, X_pdm_test = X_train[:, spatial_index + facs_index: spatial_index + facs_index + pdm_index], X_test[:, spatial_index + facs_index: spatial_index + facs_index + pdm_index]
-X_hog_train, X_hog_test = X_train[:, spatial_index + facs_index + pdm_index:], X_test[:, spatial_index + facs_index + pdm_index:]
+X_embedded_train, X_embedded_test = X_train[:, spatial_index:spatial_index + embedded_index], X_test[:, spatial_index:spatial_index + embedded_index]
+X_facs_train, X_facs_test = X_train[:, spatial_index + embedded_index:spatial_index + embedded_index + facs_index], X_test[:, spatial_index + embedded_index:spatial_index + embedded_index + facs_index]
+X_pdm_train, X_pdm_test = X_train[:, spatial_index + embedded_index + facs_index:spatial_index + embedded_index + facs_index + pdm_index], X_test[:, spatial_index + embedded_index + facs_index:spatial_index + embedded_index + facs_index + pdm_index]
+X_hog_train, X_hog_test = X_train[:, spatial_index + embedded_index + facs_index + pdm_index:], X_test[:, spatial_index + embedded_index + facs_index + pdm_index:]
 
+def create_neural_network(input_dim):
+    model = Sequential([
+        Dense(128, activation='relu', input_shape=(input_dim,)),
+        Dropout(0.5),
+        Dense(64, activation='relu'),
+        Dropout(0.5),
+        Dense(8, activation='softmax') # 8 classes
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+    # Make so that if you do model.predict_proba, it calls model.predict
+    model.predict_proba = model.predict
+    return model
 
 def spatial_relationship_model():
     # Linear scores worse individually, but better in stacking
-    svm_pipeline = Pipeline([
+    pipeline = Pipeline([
         ('scaler', StandardScaler()),
         ('svm', SVC(C=1, gamma='scale', kernel='linear', probability=True))
     ])
 
-    svm_pipeline.fit(X_spatial_train, y_train)
+    pipeline.fit(X_spatial_train, y_train)
 
 
-    return svm_pipeline
+    return pipeline
+
+
+def embedded_model():
+    pipeline = Pipeline([
+        ('scaler', StandardScaler()),
+        ('pca', PCA(n_components=0.95)),  # reduce dimensions
+        ('svm', SVC(C=1, gamma='scale', kernel='rbf', probability=True))
+    ])
+
+    pipeline.fit(X_embedded_train, y_train)
+
+    return pipeline
+
 
 
 def facial_unit_model():
-    rf = RandomForestClassifier(n_estimators=200, max_depth=None, min_samples_split=10, random_state=42)
+    """
+        rf = RandomForestClassifier(n_estimators=200, max_depth=None, min_samples_split=10, random_state=42)
 
     rf.fit(X_facs_train, y_train)
 
     return rf
 
 
+    """
+
+    nn = create_neural_network(X_facs_train.shape[1])
+    y_train_categorical = to_categorical(y_train)
+    nn.fit(X_facs_train, y_train_categorical, epochs=100, batch_size=32, verbose=0)
+
+    return nn
+
+
+
+
 def pdm_model():
-    pipeline = Pipeline([
+    """
+
+        pipeline = Pipeline([
         ('scaler', StandardScaler()),
         ('log_reg', LogisticRegression())
     ])
@@ -73,6 +134,15 @@ def pdm_model():
     pipeline.fit(X_pdm_train, y_train)
 
     return pipeline
+
+        """
+
+    nn = create_neural_network(X_pdm_train.shape[1])
+    y_train_categorical = to_categorical(y_train)
+    nn.fit(X_pdm_train, y_train_categorical, epochs=100, batch_size=32, verbose=0)
+
+    return nn
+
 
 
 def hog_model():
@@ -89,6 +159,7 @@ def hog_model():
 
 pipelines = {
     "spatial": spatial_relationship_model(),
+    "embedded": embedded_model(),
     "facs": facial_unit_model(),
     "pdm": pdm_model(),
     "hog": hog_model()
@@ -98,6 +169,7 @@ pipelines = {
 # Probabilities for each model
 probabilities = {
     "spatial": pipelines["spatial"].predict_proba(X_spatial_test),
+    "embedded": pipelines["embedded"].predict_proba(X_embedded_test),
     "facs": pipelines["facs"].predict_proba(X_facs_test),
     "pdm": pipelines["pdm"].predict_proba(X_pdm_test),
     "hog": pipelines["hog"].predict_proba(X_hog_test)
@@ -121,10 +193,13 @@ def evaluate_stacking(probabilities, pipelines):
 
     print("Accuracy of stacking classifier:", stacking_accuracy)
 
+    evaluate_results(y_test, stacking_pipeline.predict(X_stack))
+
     # Individual accuracies
     print("Accuracy of individual models")
     test_sets = {
         "spatial": X_spatial_test,
+        "embedded": X_embedded_test,
         "facs": X_facs_test,
         "pdm": X_pdm_test,
         "hog": X_hog_test
@@ -134,7 +209,10 @@ def evaluate_stacking(probabilities, pipelines):
     # evaluate_results(y_test, y_pred_spatial)
 
     for model in probabilities:
-        accuracy = pipelines[model].score(test_sets[model], y_test)
+        try:
+            accuracy = pipelines[model].score(test_sets[model], y_test)
+        except AttributeError:
+            accuracy = accuracy_score(y_test, np.argmax(probabilities[model], axis=1))
         print(f"{model} accuracy: {accuracy}")
 
 
--- src/data_processing/data_loader.py
+++ src/data_processing/data_loader.py
@@ -1,4 +1,5 @@
 import os
+from concurrent.futures import ThreadPoolExecutor
 
 import numpy as np
 
@@ -18,34 +19,25 @@ class DataLoader:
 
         self.emotions = np.array(self.load_annotations()).astype(int)
 
-        self._landmarks = []
-        self._facs_intensity = []
-        self._facs_presence = []
-        #self._landmark_distances = []
-        self._rigid_face_shape = []
-        self._nonrigid_face_shape = []
-        self._landmarks_3d = []
-        self._hog = []
-        self._deepface = []
-        self._facenet = []
-        self._vggface = []
-
-
         self.features = {
-            "landmarks": self._landmarks,
-            "facs_intensity": self._facs_intensity,
-            "facs_presence": self._facs_presence,
-            #"landmark_distances": self._landmark_distances,
-            "rigid_face_shape": self._rigid_face_shape,
-            "nonrigid_face_shape": self._nonrigid_face_shape,
-            "landmarks_3d": self._landmarks_3d,
-            "hog": self._hog,
-            "deepface": self._deepface,
-            "facenet": self._facenet,
-            "vggface": self._vggface
+            "landmarks": [],
+            "facs_intensity": [],
+            "facs_presence": [],
+            "rigid_face_shape": [],
+            "nonrigid_face_shape": [],
+            "landmarks_3d": [],
+            "hog": [],
+            "deepface": [],
+            "facenet": [],
+            "vggface": [],
+            "openface": [],
+            "sface": [],
+            "facenet512": [],
+            "arcface": [],
         }
 
-        self.load_features()
+        self.load_features_parallel()
+        print("Loaded Data")
 
     # Loads y labels
     def load_annotations(self):
@@ -101,6 +93,63 @@ class DataLoader:
                 raise ValueError(f"Missing features for {file_id}")
         return all_ids
 
+    def load_features_parallel(self):
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(self.load_feature_files, file_id) for file_id in self._ids]
+            for future in futures:
+                result = future.result()
+                for key in result:
+                    self.features[key].append(result[key])
+
+
+    def load_feature_files(self, file_id):
+        data = {}
+        features_path = f"{self.features_dir}/features/{file_id}"
+        embeddings_path = f"{self.features_dir}/embeddings/{file_id}"
+
+        if 'landmarks' not in self.exclude:
+            data['landmarks'] = np.load(f"{features_path}_landmarks.npy", mmap_mode='r')
+
+        if 'hog' not in self.exclude:
+            data['hog'] = np.load(f"{features_path}_hog.npy", mmap_mode='r')[0]
+
+        if 'deepface' not in self.exclude:
+            data['deepface'] = np.load(f"{embeddings_path}_DeepFace.npy", mmap_mode='r')
+
+        if 'facenet' not in self.exclude:
+            data['facenet'] = np.load(f"{embeddings_path}_Facenet.npy", mmap_mode='r')
+
+        if 'vggface' not in self.exclude:
+            data['vggface'] = np.load(f"{embeddings_path}_VGG-Face.npy", mmap_mode='r')
+
+        if 'openface' not in self.exclude:
+            data['openface'] = np.load(f"{embeddings_path}_OpenFace.npy", mmap_mode='r')
+
+        if 'sface' not in self.exclude:
+            data['sface'] = np.load(f"{embeddings_path}_SFace.npy", mmap_mode='r')
+
+        if 'facenet512' not in self.exclude:
+            data['facenet512'] = np.load(f"{embeddings_path}_Facenet512.npy", mmap_mode='r')
+
+        if 'arcface' not in self.exclude:
+            data['arcface'] = np.load(f"{embeddings_path}_ArcFace.npy", mmap_mode='r')
+
+
+
+        data['facs_intensity'] = np.load(f"{features_path}_facs_intensity.npy", mmap_mode='r')
+        data['facs_presence'] = np.load(f"{features_path}_facs_presence.npy", mmap_mode='r')
+        #data['landmark_distances'] = np.load(f"{features_path}_landmark_distances.npy")
+        data['rigid_face_shape'] = np.load(f"{features_path}_rigid_face_shape.npy", mmap_mode='r')
+        data['nonrigid_face_shape'] = np.load(f"{features_path}_nonrigid_face_shape.npy", mmap_mode='r')
+
+        pose = np.load(f"{features_path}_pose.npy")
+        landmarks_3d = np.load(f"{features_path}_landmarks_3d.npy", mmap_mode='r')
+        data['landmarks_3d'] = standardize_3d_landmarks(landmarks_3d, pose)
+
+        return data
+
+
+
     # Loads X features
     def load_features(self):
         # First create list of all file ids, for which we have all feature types
@@ -118,30 +167,36 @@ class DataLoader:
 
             if 'landmarks' not in self.exclude:
                 landmarks = np.load(f"{self.features_dir}/features/{file_id}_landmarks.npy")
-                self._landmarks.append(landmarks)
+                self.features['landmarks'].append(landmarks)
 
             if 'hog' not in self.exclude:
                 hog = np.load(f"{self.features_dir}/features/{file_id}_hog.npy")[0]
-                self._hog.append(hog)
+                self.features['hog'].append(hog)
 
             if 'deepface' not in self.exclude:
                 deepface = np.load(f"{self.features_dir}/embeddings/{file_id}_DeepFace.npy")
-                self._deepface.append(deepface)
+                self.features['deepface'].append(deepface)
 
             if 'facenet' not in self.exclude:
                 facenet = np.load(f"{self.features_dir}/embeddings/{file_id}_Facenet.npy")
-                self._facenet.append(facenet)
+                self.features['facenet'].append(facenet)
 
             if 'vggface' not in self.exclude:
                 vggface = np.load(f"{self.features_dir}/embeddings/{file_id}_VGG-Face.npy")
-                self._vggface.append(vggface)
+                self.features['vggface'].append(vggface)
 
-            self._facs_intensity.append(facs_intensity)
-            self._facs_presence.append(facs_presence)
-            #self._landmark_distances.append(landmark_distances)
-            self._rigid_face_shape.append(rigid_face_shape)
-            self._nonrigid_face_shape.append(nonrigid_face_shape)
-            self._landmarks_3d.append(standardized_3d_landmarks)
+            if 'openface' not in self.exclude:
+                openface = np.load(f"{self.features_dir}/embeddings/{file_id}_OpenFace.npy")
+                self.features['openface'].append(openface)
+
+            self.features['facs_intensity'].append(facs_intensity)
+            self.features['facs_presence'].append(facs_presence)
+            #self.features['landmark_distances'].append(landmark_distances)
+            self.features['rigid_face_shape'].append(rigid_face_shape)
+            self.features['nonrigid_face_shape'].append(nonrigid_face_shape)
+            self.features['landmarks_3d'].append(standardized_3d_landmarks)
+
+
 
 
 
