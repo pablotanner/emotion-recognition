d0ee750e78dd06685afaeb98c35a18944bdd2e41
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-03 12:42:56 +0200

    Update relative_gain_experiment.py

--- relative_gain_experiment.py
+++ relative_gain_experiment.py
@@ -40,30 +40,32 @@ if __name__ == '__main__':
 
     # Start with hog probabilities, then add pdm, then add landmarks_3d, then add embedded and finally facs
     #models = ['hog', 'pdm', 'landmarks_3d', 'embedded', 'facs']
-    models = ['hog', 'pdm', 'landmarks_3d', 'facs', 'embedded']
+    #models = ['hog', 'pdm', 'landmarks_3d', 'facs', 'embedded']
+    models = ['hog', 'pdm', 'landmarks_3d', 'embedded']
 
-    increased_accuracy = []
+    
+    def do_experiment():
+        increased_accuracy = []
+        for i in range(1, len(models) + 1):
+            # Print the models being used
+            logger.info(f"Using models: {models[:i]}")
+            X_stack = np.concatenate([probabilities_val[model] for model in models[:i]], axis=1)
+            stacking_pipeline.fit(X_stack, y_val)
+            # stacking_accuracy = stacking_pipeline.score(X_stack, y_val)
+            # logger.info(f"Accuracy of stacking classifier with {i} models (Validation Set): {stacking_accuracy}")
 
-    for i in range(1, len(models) + 1):
-        # Print the models being used
-        logger.info(f"Using models: {models[:i]}")
-        X_stack = np.concatenate([probabilities_val[model] for model in models[:i]], axis=1)
-        stacking_pipeline.fit(X_stack, y_val)
-        #stacking_accuracy = stacking_pipeline.score(X_stack, y_val)
-        #logger.info(f"Accuracy of stacking classifier with {i} models (Validation Set): {stacking_accuracy}")
+            balanced_accuracy = balanced_accuracy_score(y_val, stacking_pipeline.predict(X_stack))
+            logger.info(
+                f"Balanced Accuracy of stacking classifier with {i} models (Validation Set): {balanced_accuracy}")
 
-        balanced_accuracy = balanced_accuracy_score(y_val, stacking_pipeline.predict(X_stack))
-        logger.info(f"Balanced Accuracy of stacking classifier with {i} models (Validation Set): {balanced_accuracy}")
+            X_stack_test = np.concatenate([probabilities_test[model] for model in models[:i]], axis=1)
+            test_accuracy = stacking_pipeline.score(X_stack_test, y_test)
+            logger.info(f"Accuracy of stacking classifier with {i} models (Test Set): {test_accuracy}")
 
-        X_stack_test = np.concatenate([probabilities_test[model] for model in models[:i]], axis=1)
-        test_accuracy = stacking_pipeline.score(X_stack_test, y_test)
-        logger.info(f"Accuracy of stacking classifier with {i} models (Test Set): {test_accuracy}")
+            increased_accuracy.append(test_accuracy)
+            # Save
+            joblib.dump(stacking_pipeline, f'{args.experiment_dir}/stacking_pipeline_{i}.joblib')
+        logger.info(f"Accuracy increase: {increased_accuracy}")
 
-        increased_accuracy.append(test_accuracy)
-        # Save
-        joblib.dump(stacking_pipeline, f'{args.experiment_dir}/stacking_pipeline_{i}.joblib')
-
-
-    logger.info(f"Accuracy increase: {increased_accuracy}")
 
     logger.info("Experiment Finished")
