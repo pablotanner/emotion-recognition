db0330551c20c2f7794ef0135938a48ef43324a7
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-04 13:16:18 +0200

    Forgot proba in linSVC

--- concatenated_experiment.py
+++ concatenated_experiment.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     class_weights = {i: class_weights[i] for i in range(len(class_weights))}
 
     nn = NeuralNetwork(input_dim=X_train.shape[1],  class_weight=class_weights, num_epochs=20, batch_size=128)
-    linearSVC = LinearSVC(class_weight='balanced', C=0.1)
+    linearSVC = LinearSVC(class_weight='balanced', C=0.1, probability=True)
     #rf = RandomForestClassifier(n_estimators=200, max_depth=None, class_weight=class_weights)
     svm = SVC(class_weight='balanced', probability=True, kernel='rbf', C=10)
     #mlp = MLP(batch_size=128, num_epochs=30, hidden_size=256, input_size=X_train.shape[1], class_weight=class_weights, learning_rate=0.01, num_classes=8)
