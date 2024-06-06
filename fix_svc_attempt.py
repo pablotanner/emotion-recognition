92c3e77cb525c449ddce5043f4ce6e0faf7f9fab
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-06 10:09:21 +0200

    Update fixed svc with proba + cw

--- fix_svc_attempt.py
+++ fix_svc_attempt.py
@@ -2,18 +2,22 @@ import numpy as np
 from sklearn.metrics import balanced_accuracy_score
 from src.model_training import SVC
 
-X_train = np.load('train_facs_features.npy').astype(np.float32)
-y_train = np.load('y_train.npy')
-X_val = np.load('val_facs_features.npy').astype(np.float32)
-y_val = np.load('y_val.npy')
-X_test = np.load('test_facs_features.npy').astype(np.float32)
-y_test = np.load('y_test.npy')
+#X_train = np.load('train_facs_features.npy').astype(np.float32)
+#y_train = np.load('y_train.npy')
+#X_val = np.load('val_facs_features.npy').astype(np.float32)
+#y_val = np.load('y_val.npy')
+#X_test = np.load('test_facs_features.npy').astype(np.float32)
+#y_test = np.load('y_test.npy')
+
+X_train = np.random.rand(1000, 100).astype(np.float32)
+y_train = np.random.randint(0, 5, 1000)
+
 
 svc = SVC(C=1, probability=True, kernel='rbf', class_weight='balanced')
 
 svc.fit(X_train, y_train)
 
-balanced_accuracy = balanced_accuracy_score(y_val, svc.predict(X_val))
+#balanced_accuracy = balanced_accuracy_score(y_val, svc.predict(X_val))
 
-print(f"Balanced Accuracy on Validation: {balanced_accuracy}")
+#print(f"Balanced Accuracy on Validation: {balanced_accuracy}")
 
--- src/model_training/proba_svc.py
+++ src/model_training/proba_svc.py
@@ -22,15 +22,24 @@ def _fit_proba(self, X, y, sample_weight=None):
         raise RuntimeError(
             "Scikit-learn is needed to use SVM probabilities")
 
+    # Currently, fitting a probabilistic SVC with class weights requires at least 3 classes, otherwise the following, ambiguous error is raised:
+    # ValueError: Buffer dtype mismatch, expected 'const float' but got 'double'
+    if len(set(y)) < 3:
+        raise ValueError("At least 3 classes are required to use probabilistic SVC with class weights.")
+
     self.prob_svc = CalibratedClassifierCV(SVC(**params),
                                            cv=5,
                                            method='sigmoid')
 
-
     sample_weight = apply_class_weight(self.handle, sample_weight, self.class_weight, y, self.verbose, self.output_type, self.dtype)
 
+    # If sample_weight is not None, it is a cupy array, and we need to convert it to a numpy array for skleran
+    if sample_weight is not None:
+        # Convert cupy array to numpy array
+        sample_weight = sample_weight.get()
     with cuml.internals.exit_internal_api():
-        self.prob_svc.fit(X, y, sample_weight=sample_weight.get())
+        # Fit the model, sample_weight is either None or a numpy array
+        self.prob_svc.fit(X, y, sample_weight=sample_weight)
     self._fit_status_ = 0
     return self
 
