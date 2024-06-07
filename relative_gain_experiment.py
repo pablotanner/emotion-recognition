11ee5b8bba776ff2d3dd5fbfda23043e913fda30
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-07 12:42:27 +0200

    Update relative_gain_experiment.py

--- relative_gain_experiment.py
+++ relative_gain_experiment.py
@@ -1,4 +1,5 @@
 import argparse
+import itertools
 import logging
 import joblib
 from cuml.preprocessing import StandardScaler
@@ -15,7 +16,7 @@ if __name__ == '__main__':
         description='Training one classifier with all feature types')
     parser.add_argument('--experiment-dir', type=str, help='Directory to checkpoint file',
                         default='/local/scratch/ptanner/relative_gain_experiments')
-    parser.add_argument('--use-concat', action='store_true', help='Use concatenated probabilities', default=False)
+    #parser.add_argument('--use-concat', action='store_true', help='Use concatenated probabilities', default=False)
 
     args = parser.parse_args()
 
@@ -32,13 +33,9 @@ if __name__ == '__main__':
     probabilities_val = np.load('/local/scratch/ptanner/hybrid_fusion_experiments/probabilities_val.npy', allow_pickle=True).item()
     probabilities_test = np.load('/local/scratch/ptanner/hybrid_fusion_experiments/probabilities_test.npy', allow_pickle=True).item()
 
-    if args.use_concat:
-        proba_val_concat = np.load('concat_proba_nn_val.npy')
-        proba_test_concat = np.load('concat_proba_nn_test.npy')
 
-        probabilities_val['concat'] = proba_val_concat
-        probabilities_test['concat'] = proba_test_concat
-
+    probabilities_val['concat'] = np.load('/local/scratch/ptanner/concatenated_experiment/probabilities_val.npy', allow_pickle=True).item()['SVC']
+    probabilities_test['concat'] = np.load('/local/scratch/ptanner/concatenated_experiment/probabilities_val.npy', allow_pickle=True).item()['SVC']
 
     y_val = np.load('y_val.npy')
     y_test = np.load('y_test.npy')
@@ -50,11 +47,9 @@ if __name__ == '__main__':
     ])
 
     # Start with hog probabilities, then add pdm, then add landmarks_3d, then add embedded and finally facs
-    models = ['hog', 'pdm', 'landmarks_3d', 'embedded', 'facs']
+    models = ['hog', 'pdm', 'landmarks_3d', 'embedded', 'facs', 'concat']
 
-    if args.use_concat:
-        # Place concat at first position
-        models = ['concat'] + models
+
 
     def do_experiment():
         increased_accuracy = []
@@ -78,19 +73,50 @@ if __name__ == '__main__':
             # Save
             joblib.dump(stacking_pipeline, f'{args.experiment_dir}/stacking_pipeline_{i}.joblib')
 
-
-
         logger.info(f"Accuracy increase: {increased_accuracy}")
+
+
+    def do_experiment_subset():
+        increased_accuracy = []
+
+        # Generate all non-empty subsets of models
+        for subset_size in range(1, len(models) + 1):
+            for subset in itertools.combinations(models, subset_size):
+                # Generate all permutations of the current subset
+                for permutation in itertools.permutations(subset):
+                    logger.info(f"Using models: {permutation}")
+                    X_stack = np.concatenate([probabilities_val[model] for model in permutation], axis=1)
+                    stacking_pipeline.fit(X_stack, y_val)
+
+                    #balanced_accuracy = balanced_accuracy_score(y_val, stacking_pipeline.predict(X_stack))
+                    #logger.info(
+                       # f"Balanced Accuracy of stacking classifier with models {permutation} (Validation Set): {balanced_accuracy}")
+
+                    X_stack_test = np.concatenate([probabilities_test[model] for model in permutation], axis=1)
+                    test_accuracy = stacking_pipeline.score(X_stack_test, y_test)
+                    logger.info(
+                        f"Accuracy of stacking classifier with models {permutation} (Test Set): {test_accuracy}")
+
+                    increased_accuracy.append((permutation, test_accuracy))
+
+                    # Save the model
+                    # joblib.dump(stacking_pipeline,
+                               # f'{args.experiment_dir}/stacking_pipeline_{"_".join(permutation)}.joblib')
+
+        # Optionally, you could return or save the increased_accuracy list to analyze later
+        return increased_accuracy
 
     #do_experiment()
 
-    X_stack = np.concatenate([probabilities_val[model] for model in models], axis=1)
-    X_stack_test = np.concatenate([probabilities_test[model] for model in models], axis=1)
-    stacking_pipeline.fit(X_stack, y_val)
-    explainer = shap.Explainer(stacking_pipeline.named_steps['log_reg'], X_stack)
-    shap_values_test = explainer(X_stack_test)
+    increased_accuracy = do_experiment_subset()
 
-    shap.bar_plot(shap_values_test, max_display=10)
+    #X_stack = np.concatenate([probabilities_val[model] for model in models], axis=1)
+    #X_stack_test = np.concatenate([probabilities_test[model] for model in models], axis=1)
+    #stacking_pipeline.fit(X_stack, y_val)
+    #explainer = shap.Explainer(stacking_pipeline.named_steps['log_reg'], X_stack)
+    #shap_values_test = explainer(X_stack_test)
+
+    #shap.bar_plot(shap_values_test, max_display=10)
 
 
     logger.info("Experiment Finished")
