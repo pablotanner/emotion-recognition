83a9bc17727c9d172241a73a684737311a3bfef8
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-09 10:35:03 +0200

    Add more useful SHAP violin plot visualziation

--- src/visualizations/shap_visualization.py
+++ src/visualizations/shap_visualization.py
@@ -9,11 +9,11 @@ from matplotlib import pyplot as plt
 
 emotions = ['Neutral', 'Happy', 'Sad', 'Surprise', 'Fear', 'Disgust', 'Angry', 'Contempt']
 
-shap_values = joblib.load('shap_test.joblib')[:, :, 1]
-#shap_values = joblib.load('SV_test_correct.joblib')[:, :, 1]
+#shap_values = joblib.load('shap_test.joblib')[:, :, 1]
+shap_values = joblib.load('SV_test_correct.joblib')[:, :, 1]
 
-models = ['HOG', 'PDM', '3D LND', 'Embeddings', 'FAUs']
-#models = ['HOG', 'PDM', 'Embeddings', 'FAUs']
+#models = ['HOG', 'PDM', '3D LND', 'Embeddings', 'FAUs']
+models = ['HOG', 'PDM', 'Embeddings', 'FAUs']
 
 
 real_feature_names = []
@@ -23,69 +23,96 @@ for model in models:
 
 shap_values.feature_names = real_feature_names
 
-def get_mean_abs_shap_values(shap_values):
-    return np.mean(np.abs(shap_values))
 
-# {feature: mean_abs_shap_value}
-features = {f: get_mean_abs_shap_values(shap_values[:, i].values) for i, f in enumerate(real_feature_names)}
+def bar_plot():
+    def get_mean_abs_shap_values(shap_values):
+        return np.mean(np.abs(shap_values))
 
-# Dictionary of features and as value array of their shap values (one value per emotion)
-feature_shap_values = {m: [] for m in models}
+    # {feature: mean_abs_shap_value}
+    features = {f: get_mean_abs_shap_values(shap_values[:, i].values) for i, f in enumerate(real_feature_names)}
 
-for model in models:
-    for emotion in emotions:
-        feature_shap_values[model].append(features[model + ' ' + emotion])
+    # Dictionary of features and as value array of their shap values (one value per emotion)
+    feature_shap_values = {m: [] for m in models}
 
+    for model in models:
+        for emotion in emotions:
+            feature_shap_values[model].append(features[model + ' ' + emotion])
 
-# Create data plot
-df = pd.DataFrame(feature_shap_values, index=emotions).T
+    # Create data plot
+    df = pd.DataFrame(feature_shap_values, index=emotions).T
 
-colors = plt.get_cmap('tab20b')(np.linspace(0, 1, 8))
-ax = df.plot.barh(stacked=True, figsize=(14, 7), color=colors, align='center')
-plt.xlabel('mean( |SHAP Value| )', fontsize=18)
-plt.ylabel('Feature Type', fontsize=18)
+    colors = plt.get_cmap('tab20b')(np.linspace(0, 1, 8))
+    ax = df.plot.barh(stacked=True, figsize=(14, 7), color=colors, align='center')
+    plt.xlabel('mean( |SHAP Value| )', fontsize=18)
+    plt.ylabel('Feature Type', fontsize=18)
 
-# add grid
-plt.grid(axis='x', color='white', linestyle='-')
+    # add grid
+    plt.grid(axis='x', color='white', linestyle='-')
 
-# Make sure grid is behind bars
-ax.set_axisbelow(True)
+    # Make sure grid is behind bars
+    ax.set_axisbelow(True)
 
+    # Create annotations
+    for p in ax.patches:
+        width, height = p.get_width(), p.get_height()
+        x, y = p.get_xy()
+        if not width > 0.02:
+            continue
+        ax.text(x + width / 2,
+                y + height / 2,
+                '{:.2f}'.format(width),
+                horizontalalignment='center',
+                verticalalignment='center',
+                color='white',
+                fontsize=14)
 
-#Create annotations
-for p in ax.patches:
-    width, height = p.get_width(), p.get_height()
-    x, y = p.get_xy()
-    if not width > 0.02:
-        continue
-    ax.text(x + width / 2,
-            y + height / 2,
-            '{:.2f}'.format(width),
-            horizontalalignment='center',
-            verticalalignment='center',
-            color='white',
-            fontsize=14)
+    legend = plt.legend(loc='center',
+                        frameon=False,
+                        bbox_to_anchor=(0., 1.10, 1., .102),
+                        mode='expand',
+                        ncol=4,
+                        borderaxespad=-.46,
+                        prop={'size': 15, 'family': 'Calibri'})
 
+    for text in legend.get_texts():
+        plt.setp(text, color='black')
 
-legend = plt.legend(loc='center',
-       frameon=False,
-       bbox_to_anchor=(0., 1.10, 1., .102),
-       mode='expand',
-       ncol=4,
-       borderaxespad=-.46,
-       prop={'size': 15, 'family':'Calibri'})
+    title = plt.title('Feature Type Importances by Emotion', fontsize=20)
 
-for text in legend.get_texts():
-    plt.setp(text, color='black')
+    # add space above
+    plt.subplots_adjust(top=0.8)
 
-title = plt.title('Feature Type Importances by Emotion', fontsize=20)
+    # add gray background
+    plt.gca().set_facecolor('lightgray')
 
-# add space above
-plt.subplots_adjust(top=0.8)
+    # plt.savefig('shap_values.png')
+    plt.show()
 
+def violin_plot():
+    features = {f: shap_values[:, i].values for i, f in enumerate(real_feature_names)}
 
-# add gray background
-plt.gca().set_facecolor('lightgray')
+    plt.figure(figsize=(14, 9))
 
-#plt.savefig('shap_values.png')
-plt.show()
+    fig, axis = plt.subplots(1, 4, figsize=(14, 7))
+
+    for i, model in enumerate(models):
+        for j in range(8):
+            axis[i].violinplot(features[model + ' ' + emotions[j]], vert=False, positions=[j], showmeans=False,
+                               showmedians=True)
+            axis[i].set_yticks(range(8))
+            axis[i].set_yticklabels(emotions)
+            axis[i].set_title(model, fontsize=16)
+            axis[i].grid(True)
+            # Make all have same x-axis
+            axis[i].set_xlim(-0.5, 0.5)
+            axis[i].set_xlabel('SHAP Value')
+
+    plt.subplots_adjust(wspace=0.55, top=0.85)
+
+    plt.suptitle('SHAP Value Distribution by Feature Type', fontsize=20, y=0.95)
+
+    plt.savefig('shap_values_violin.png')
+
+    plt.show()
+
+violin_plot()
