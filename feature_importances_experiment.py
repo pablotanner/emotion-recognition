5a3b4a85afe3a7a0752ca495da5933af4fc5b738
Pablo Tanner <pablo.tanner@uzh.ch>
2024-06-16 15:03:29 +0200

    Update args for shap extract

--- feature_importances_experiment.py
+++ feature_importances_experiment.py
@@ -6,6 +6,7 @@ import joblib
 import numpy as np
 #import shap
 from cuml.explainer import PermutationExplainer, KernelExplainer
+from sklearn.model_selection import train_test_split
 from src.model_training import SVC
 from cuml.svm import LinearSVC
 from src.util.data_paths import get_data_path
@@ -14,10 +15,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Feature Importance Experiment')
     parser.add_argument('--feature', type=str, help='Feature Type',
                         default='facs')
+    parser.add_argument('--clf', type=str, help='Classifier')
+    parser.add_argument('--exp', type=str, help='Kernel Explainer or Permutation Explainer')
+
+
     experiment_dir = '/local/scratch/ptanner/feature_importance_experiments'
     pca_dir = '/local/scratch/ptanner/concatenated_experiment/pca_models'
 
     args = parser.parse_args()
+
+    feature = args.feature
+    clf = args.clf
+    exp = args.exp
+    
 
     logger = logging.getLogger(__name__)
     logging.basicConfig(level=logging.INFO,
@@ -29,43 +39,47 @@ if __name__ == '__main__':
 
     # Might be PCA Reduced
     X_train = np.load(get_data_path('train', args.feature)).astype(np.float32)
+    X_test = np.load(get_data_path('test', args.feature)).astype(np.float32)
     y_train = np.load('y_train.npy')
 
-    X_test = np.load(get_data_path('test', args.feature)).astype(np.float32)
+    #X = np.load(get_data_path('test', args.feature)).astype(np.float32)
+    #y = np.load('y_test.npy')
+
+    # Split into Train and Test
+
+    #X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Coincidentally, all 3 (4 with concat) feature types have same SVC RBF params
-    if os.path.exists(f'{experiment_dir}/{args.feature}_classifier.joblib'):
-        logger.info(f"Classifier for {args.feature} already exists. Loading")
-        svc = joblib.load(f'{experiment_dir}/{args.feature}_classifier.joblib')
+    if os.path.exists(f'{experiment_dir}/{args.feature}_{clf}.joblib'):
+        logger.info(f"Classifier {clf} for {args.feature} already exists. Loading")
+        svc = joblib.load(f'{experiment_dir}/{args.feature}_{clf}.joblib')
     else:
-        logger.info(f"Training SVC on {args.feature} features")
-        svc = LinearSVC(C=1, kernel='rbf', class_weight='balanced')
+        logger.info(f"Training {clf} on {args.feature} features")
+        if clf == 'svc':
+            svc = SVC(C=1, kernel='rbf', class_weight='balanced')
+        else:
+            svc = LinearSVC(C=1, class_weight='balanced')
         # Fit SVC
         svc.fit(X_train, y_train)
-        joblib.dump(svc, f'{experiment_dir}/{args.feature}_classifier.joblib')
+        joblib.dump(svc, f'{experiment_dir}/{args.feature}_{clf}.joblib')
 
 
-    if os.path.exists(f'{experiment_dir}/{args.feature}_explainer.joblib'):
-        logger.info(f"Explainer for {args.feature} already exists. Loading")
-        explainer = joblib.load(f'{experiment_dir}/{args.feature}_explainer.joblib')
+    if os.path.exists(f'{experiment_dir}/{args.feature}_{exp}.joblib'):
+        logger.info(f"Explainer {exp} for {args.feature} already exists. Loading")
+        explainer = joblib.load(f'{experiment_dir}/{args.feature}_{exp}.joblib')
     else:
         logger.info(f"Generating Explainer")
         # Generate Explainer
-        #explainer = PermutationExplainer(model=svc.predict, data=X_train, random_state=42)
+        explainer = PermutationExplainer(model=svc.predict, data=X_train, random_state=42, is_gpu_model=True, dtype=np.float32)
         #explainer = shap.Explainer(svc.predict, X_train)
-        explainer = KernelExplainer(
-            model=svc.predict,
-            data=X_train,
-            is_gpu_model=True,
-            random_state=42,
-            dtype=np.float32
-        )
-        joblib.dump(explainer, f'{experiment_dir}/{args.feature}_explainer.joblib')
+        #explainer = KernelExplainer(model=svc.predict,data=X_train,is_gpu_model=True,random_state=42,dtype=np.float32)
+        joblib.dump(explainer, f'{experiment_dir}/{args.feature}_{exp}.joblib')
 
     # Delete Unnecessary Variables
     del X_train, y_train, svc
 
     logger.info(f"Generating SHAP Values")
+
     # Get SHAP Values
     shap_values = explainer(X_test)
 
