73d9431604308c1161ceb4d42a1755c9137c00aa
Pablo Tanner <pablo.tanner@uzh.ch>
2024-07-02 14:52:23 +0200

    Update individual_experiments.py

--- individual_experiments.py
+++ individual_experiments.py
@@ -138,7 +138,7 @@ if __name__ == '__main__':
 
 
     for clf_name, clf_class in classifiers.items():
-        if clf_name != 'RandomForest':
+        if clf_name != 'RandomForest' and args.feature == 'embeddings':
             continue
         logger.info(f'Running experiments for classifier {clf_name}')
         param_grid = list(ParameterGrid(parameters[clf_name]))
